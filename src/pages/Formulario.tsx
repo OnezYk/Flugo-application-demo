be9055bf3cd3f9422785552892150cfb34f4ffb7
Enzo Yokota <Enzoribasprofissional@gmail.com>
2026-03-10 21:56:23 -0300

    Avanços em fomulario, finalizadas algumas animacoes e preparacao para coneccoes DB via props.

--- src/pages/Formulario.tsx
+++ src/pages/Formulario.tsx
@@ -1,62 +1,41 @@
-import { Box, Button, Typography } from '@mui/material'
+// MUI
+import { Box, Button, TextField, Typography } from '@mui/material'
+
+// MUI Icons
 import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
-import { useState } from 'react'
+
+// Hooks
+import { useState, type ReactNode } from 'react'
+import MUISwitch from '../components/Switch';
+
+type FormData ={
+  nome: string
+  email: string
+  departamento: string
+  status: boolean
+}
+
+type PassoProps = {
+  passo: number
+  form?: FormData
+  setForm?: React.Dispatch<React.SetStateAction<FormData>>
+  children?: React.ReactNode
+}
 
 const Formulario = () => {
-  const [step, setStep] = useState(1)
+  const [passo, setPasso] = useState(1)
   const [form, setForm] = useState({
     nome: '',
     email: '',
     departamento: '',
-    status: 'Ativo'
+    status: false
   })
 
   return (
     <>
-      <Box>
-        <stepCounter passo={step}/>
-        {step === 1 && <PassoUm passo={step}/>}
-        {step === 2 && <PassoDois />}
-        {step === 3 && <PassoTres />}
-      </Box>
-
-      <Button onClick={() => setStep(step + 1)}>Próximo</Button>
-      {step > 1 && <Button onClick={() => setStep(step - 1)}>Voltar</Button>}
-    </>
-  )
-}
-
-const PassoUm = ({passo} : {passo:number}) => {
-
-  return (
-    <PassoFrame passo={passo}></PassoFrame>
-  )
-
-}
-
-const PassoDois = ({passo} : {passo:number}) => {
-
-  return (
-    <PassoFrame passo={passo}></PassoFrame>
-  )
-
-}
-
-const PassoTres = ({passo} : {passo:number}) => {
-
-  return (
-    <PassoFrame passo={passo}></PassoFrame>
-  )
-
-}
-
-const PassoFrame = ({passo}: {passo:number}) => {
-
-  console.log(passo)
-
-  return (
       <Box sx={{
         m: 4,
+        mr: 5,
         display: 'flex',
         flexDirection: 'column',
         color: 'text.primary',
@@ -74,16 +53,29 @@ const PassoFrame = ({passo}: {passo:number}) => {
         <Box sx={{
           mt: 2,
           display: 'grid',
-          gridTemplateColumns: '93% 6%',
+          gridTemplateColumns: '97% 1fr',
           gap: 2,
           alignItems: 'center',
           justifyContent: 'space-around'
         }}>
           <Box sx={{
+            position: 'relative',
             borderRadius: 2,
             height: '4px',
             bgcolor: 'success.light'
-          }}></Box>
+          }}>
+
+            <Box sx={{
+              height: '100%',
+              position: 'absolute',
+              bgcolor: 'success.main',
+              transition: 'all 1s',
+              width: `${passo == 1 ? '0%' : passo == 2 ? '50%' : '100%' }`
+            }}>
+
+            </Box>
+
+          </Box>
 
           <Typography> 0% </Typography>
 
@@ -95,7 +87,135 @@ const PassoFrame = ({passo}: {passo:number}) => {
           mt: 4
         }}>
 
-          <Box sx={{
+        <StepTab passo={passo}/>
+        {passo === 1 && <PassoUm form={form} setForm={setForm} passo={passo}/>}
+        {passo === 2 && <PassoDois passo={passo}/>}
+        {passo === 3 && <PassoTres passo={passo}/>}
+
+        </Box>
+
+      </Box>
+      <Box>
+      </Box>
+
+      <Button onClick={() => setPasso(passo + 1)}>Próximo</Button>
+      {passo > 1 && <Button onClick={() => setPasso(passo - 1)}>Voltar</Button>}
+    </>
+  )
+}
+
+//Seção de passos:
+const PassoUm = ({form, setForm} : PassoProps) => {
+
+  return (
+    <PassoFrame title='Infos Básicas'>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 3
+      }}>
+        <InputField
+          info="Título"
+          value={form!.nome}
+          onChange={(e) => setForm!(prev => ({ ...prev, nome: e.target.value }))}/>
+        <InputField
+          info="E-mail"
+          value={form!.email}
+          onChange={(e) => setForm!(prev => ({ ...prev, email: e.target.value }))}/>
+      </Box>
+
+      <Box sx={{
+        mt: 5,
+        display: 'flex',
+        alignItems: 'center',
+        gap: 2
+      }}>
+        <MUISwitch/>
+        <Typography>Ativar ao criar</Typography>
+      </Box>
+
+    </PassoFrame>
+  )
+
+}
+
+const PassoDois = ({form, setForm} : PassoProps) => {
+
+  return (
+    <PassoFrame title='Infos Profissionais'>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 3
+      }}>
+        <InputField
+          info="Título"
+          value={form!.nome}
+          onChange={(e) => setForm!(prev => ({ ...prev, nome: e.target.value }))}/>
+      </Box>
+
+    </PassoFrame>
+  )
+
+}
+
+const PassoTres = ({passo} : PassoProps) => {
+
+  return (
+    <PassoFrame passo={passo}></PassoFrame>
+  )
+
+}
+//-----------------------------------------------------
+
+//Corpo principal do formulário
+const PassoFrame = ({children, title} : {children: ReactNode, title: string}) => {
+
+  return (
+    <Box sx={{
+      minHeight: '500px',
+    }}> 
+    
+      <Typography sx={{
+        position: 'relative',
+        bottom: 3,
+        mb:3,
+        fontWeight: 600,
+        fontSize: 25,
+        color: 'text.secondary'
+      }}> 
+      {title}
+      </Typography> 
+
+      {children}
+
+    </Box>
+  )
+
+}
+
+// Input de infos
+const InputField = ({info, value, onChange} : {
+  info: string, 
+  value: string, 
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}) => {
+  return (
+    <TextField
+      label={info}
+      variant="outlined"
+      fullWidth
+      value={value}
+      onChange={onChange}
+    />
+  )
+}
+
+// Barra lateral do formulário
+const StepTab = ({passo} : PassoProps) => {
+
+  return (
+              <Box sx={{
             display: 'flex',
             flexDirection: 'column',
             gap: 1,
@@ -140,51 +260,65 @@ const PassoFrame = ({passo}: {passo:number}) => {
           }
 
 
-            <Typography sx={{
-              fontWeight: '600',
-              fontSize: '15px'
-            }}>Infos Básicas</Typography>
+          <Typography sx={{
+            fontWeight: '600',
+            fontSize: '15px'
+          }}>Infos Básicas</Typography>
 
           </Box>
           
           <Box sx={{
-            bgcolor: 'text.disabled',
+            bgcolor: `${passo == 1 ? 'text.disabled' : passo == 2 ? 'text.secondary' : 'success.main'}`,
             width: 2,
-            minHeight: `${passo == 1 ? '100px' : '50px'}`,
+            minHeight: `${passo == 1 ? '100px' : passo == 2 ? '50px' : '0px'}`,
             ml: 1.5,
             transition: 'all 1s ease'
           }}></Box>
 
             <Box sx={{
-              width: 32,
-              height: 32,
               display: 'flex',
-              alignItems: 'center'
+              alignItems: 'center',
+              gap: 2
             }}>
-
-                <Typography sx={{
-                  display: 'inline-flex',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  bgcolor: `${passo == 1 ? 'text.disabled' : 'success.main'}`,
-                  color:  `${passo == 1 ? 'text.secondary' : 'primary.contrastText'}`,
-                  fontWeight: '600',
-                  borderRadius: '50%',
-                  width: 27,
-                  height: 27,
-                  textAlign: 'center',
-                }}>2</Typography> 
-                
+              <Box sx={{
+                width: 32,
+                height: 32,
+                display: 'flex',
+                alignItems: 'center'
+              }}>
+                {passo < 3 ?                   
+                  <Typography sx={{
+                    display: 'inline-flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    bgcolor: `${passo == 1 ? 'text.disabled' : 'success.main'}`,
+                    color:  `${passo == 1 ? 'text.secondary' : 'primary.contrastText'}`,
+                    fontWeight: '600',
+                    borderRadius: '50%',
+                    width: 27,
+                    height: 27,
+                    textAlign: 'center',
+                    transition: 'all 1s'
+                  }}>2</Typography>
+                :
+                <CheckCircleRoundedIcon sx={{
+                  fontSize: 32,
+                  position: 'relative',
+                  right: 2,
+                  color: 'success.main'
+                }}/>
+                }
+              
+              </Box>
+              <Typography sx={{
+                fontWeight: '600',
+                fontSize: '15px'
+              }}>Infos Profissionais</Typography>
             </Box>
 
           </Box>
-
-          <Box>RECEBIDO COMO CHILDREN</Box>
-
-        </Box>
-
-      </Box>
   )
 
 }
+
 export default Formulario
